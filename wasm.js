610b242ae4f6c5e00dc034a66a9f9fac09a5c024
Kiril Vatev <vatev.1@gmail.com>
2023-11-07 01:43:21 -0500

    removing unnecessary comment

--- wasm.js
+++ wasm.js
@@ -4,27 +4,3 @@ const fs = require('fs');
 const wasmBinary = fs.readFileSync('./libheif-wasm/libheif.wasm');
 
 module.exports = require('./libheif-wasm/libheif.js')({ wasmBinary });
-
-// NOTE: for webpack, you need to do something like:
-/*
-
-import libheif from './libheif-wasm/libheif.js'
-import wasmBinary from './libheif-wasm/libheif.wasm'
-
-export default libheif({ wasmBinary });
-
-*/
-
-// then add this to rules:
-/*
-
-module.exports = {
-  module: {
-    rules: [{
-      test: /\.wasm$/,
-      type: "asset/inline",
-    }]
-  },
-};
-
-*/
